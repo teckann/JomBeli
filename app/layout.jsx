d948491fd681168912ce3e68b67ad03a5359d551
Gan Teck Ann <ganteckann582006@gmail.com>
2026-06-06 22:38:50 +0800

    feat: login design (in progress)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,12 @@
 import "@/app/_styles/globals.css";
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider } from "next-themes";
+import "@/app/_styles/globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <div>
-          <main>
-            <ThemeProvider>
-              {children}
-            </ThemeProvider>
-          </main>
-        </div>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,13 +1,29 @@
 import SignInForm from "../_components/SignInForm";
+import Image from "next/image";
+
+import styles from "./page.module.css";
 
 async function page({ searchParams }) {
   const { error, message } = await searchParams;
 
   return (
-    <div>
-      <h1>Sign In</h1>
-      <SignInForm />
-    </div>
+    <main className={styles.main}>
+      <div className={styles.card}>
+        <div className={styles.landing_img_container}>
+          <Image
+            className={styles.landing_img}
+            src="/landing-3d.png"
+            alt="Signin Landing Image"
+            fill
+            priority
+          />
+        </div>
+
+        <div className={styles.form_container}>
+          <SignInForm />
+        </div>
+      </div>
+    </main>
   );
 }
 
