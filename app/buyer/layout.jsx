6543d1e21560c4f392a882284b8b0ef6b839a529
belugawhale0912 <leon0912tan@gmail.com>
2026-06-14 13:30:22 +0800

    fix: removed duplicated import in layouts

--- app/buyer/layout.jsx
+++ app/buyer/layout.jsx
@@ -1,6 +1,5 @@
 import "@/app/_styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import "@/app/_styles/globals.css";
 import BuyerNavBar from "../_components/BuyerNavBar/BuyerNavBar";
 
 export default function RootLayout({ children }) {
