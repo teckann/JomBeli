1ed1af7cfdfb957b007b6b34c9b3ff323ee4102a
belugawhale0912 <leon0912tan@gmail.com>
2026-06-14 13:53:43 +0800

    fix: hydration error caused by theme toggle button

--- app/_components/ThemeToggleButton.jsx
+++ app/_components/ThemeToggleButton.jsx
@@ -1,9 +1,19 @@
 "use client"
 
 import { useTheme } from 'next-themes'
+import { useEffect } from 'react';
 
 export default function ThemeToggleButton(){
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true) 
+  },[])
+
+  if(!mounted){
+    return null //render null when page is not mounted to prevent hydration error
+  }
 
   return (
     <div>
