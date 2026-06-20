b398a99eba996d904d9a9189204442589591b780
belugawhale0912 <leon0912tan@gmail.com>
2026-06-20 16:43:03 +0800

    feat: improved faq page styling

---
+++ app/buyer/helpcentre/FAQ.js
@@ -0,0 +1,49 @@
+export const faqData = [
+  // --- SHIPPING & DELIVERY ---
+  {
+    id: "ship-01",
+    question: "How long does shipping take?",
+    answer: "Your parcel will be shipped to you within 24 hours"
+  },
+  {
+    id: "ship-02",
+    question: "Do you offer free shipping?",
+    answer: "Free shipping vouchers will be given by the platform, stay tuned to our anouncement for free shipping campaigns"
+  },
+  {
+    id: "ship-03",
+    question: "Do you ship internationally?",
+    answer: "We currently only provide service in KL area to provide instant shipping services, we will expand our services in the future"
+  },
+  {
+    id: "order-01",
+    question: "How can I track my order?",
+    answer: "You can track your shipment directly from your Account Dashboard under 'Order History'."
+  },
+  {
+    id: "order-02",
+    question: "Can I change or cancel my order after placing it?",
+    answer: "Because we process orders quickly to ensure fast delivery, changes or cancellations can only be made within 30 minutes of placing the order. Please contact our support team immediately via live chat."
+  },
+  {
+    id: "return-01",
+    question: "What is your return policy?",
+    answer: "We offer a 30-day hassle-free return policy. Items must be unworn, unwashed, and in their original packaging with tags attached. Return shipping is free for domestic orders."
+  },
+  {
+    id: "return-02",
+    question: "How long does it take to get my refund?",
+    answer: "Once we receive and inspect your returned item at our warehouse, your refund will be processed back to your original payment method within 5-7 business days."
+  },
+
+  {
+    id: "pay-01",
+    question: "What payment methods do you accept?",
+    answer: "We accept payment VIA our platform's e-wallet platform and cash on delivery (COD) for orders under RM500"
+  },
+  {
+    id: "pay-02",
+    question: "Is my payment information secure?",
+    answer: "Absolutely. We use industry-standard SSL encryption to protect your data. Your credit card information is tokenized and never stored directly on our servers."
+  }
+];
