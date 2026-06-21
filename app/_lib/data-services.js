6c445ef2fffceedd2c7d5047c36f2039f0d56f63
Jimmylim69 <limjinming0609@gmail.com>
2026-06-22 02:48:56 +0800

    fixing data-services

--- app/_lib/data-services.js
+++ app/_lib/data-services.js
@@ -60,3 +60,146 @@ export async function getWalletBalance(id) {
 
   return data;
 }
+
+export async function getTransactionMonths(id) {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("WALLET_TRANSACTIONS_T")
+    .select("created_at")
+    .eq("user_id", id)
+    .order("created_at", { ascending: true });
+
+  if (error) {
+    console.error(error);
+    return [];
+  }
+
+  // month mapping (after filter)
+  const monthsMap = new Map();
+
+  for (const item of data) {
+    const created_at = item.created_at;
+    const date = new Date(created_at);
+
+    const year = date.getFullYear();
+    // [0-11]  0=Jan,...，11=Dec
+    const month = date.getMonth();
+
+    // format the pair (2026 Jan = 2026-0)
+    const key = `${year}-${month}`;
+
+    // filter out the repeat months
+    if (!monthsMap.has(key)) {
+      // key-value (key = 2026-0, value = {year: 2026, month: 0})
+      monthsMap.set(key, { year, month });
+    }
+  }
+
+  // sorting logic
+  const months = Array.from(monthsMap.values()).sort(
+    (a, b) => new Date(b.year, b.month) - new Date(a.year, a.month),
+  );
+
+  // check if there have another years
+  // filter out month, just keep year
+  const uniqueYears = [...new Set(months.map((m) => m.year))];
+  const isMultiYear = uniqueYears.length > 1;
+
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  // format the data
+  const result = months.map(({ year, month }) => {
+    return {
+      value: `${year}-${month + 1}`,
+      label: isMultiYear ? `${monthNames[month]} ${year}` : monthNames[month],
+    };
+  });
+
+  return result;
+}
+
+export async function getTransactions(id) {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("WALLET_TRANSACTIONS_T")
+    .select("*")
+    .eq("user_id", id)
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.error("Failed to fetch transaction records:", error.message);
+    throw new Error("Could not fetch transaction records");
+  }
+
+  return data;
+}
+
+export async function getFilterTransactions(id, month, type) {
+  const supabase = await createClient();
+
+  let query = supabase
+    .from("WALLET_TRANSACTIONS_T")
+    .select("*")
+    .eq("user_id", id);
+
+  // type filtering
+  if (type && type !== "all") {
+    query = query.eq("direction", type);
+  }
+
+  // month filtering (2026-1 || 2025-12)
+  if (month && month !== "all") {
+    const [year, m] = month.split("-");
+    const safeMonth = `${year}-${m.padStart(2, "0")}`;
+
+    const start = new Date(`${safeMonth}-01T00:00:00Z`);
+    const end = new Date(start);
+    end.setMonth(end.getMonth() + 1);
+
+    query = query
+      .gte("created_at", start.toISOString())
+      .lt("created_at", end.toISOString());
+  }
+
+  // sorting
+  query = query.order("created_at", { ascending: false });
+
+  const { data, error } = await query;
+
+  if (error) {
+    console.error("Failed to fetch transaction records:", error.message);
+    throw new Error("Could not fetch transaction records");
+  }
+
+  return data || [];
+}
+
+
+export async function getSellerVoucher(id) {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("VOUCHERS_T")
+    .select("*")
+    .eq("user_id", id);
+
+  if (error) {
+    console.error("Failed to fetch balance:", error.message);
+    throw new Error("Could not fetch balance");
+  }
+
+  return data;
+}
