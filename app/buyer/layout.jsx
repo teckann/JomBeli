76a8d608a852c65d934ad276f82620b7fc97de69
Gan Teck Ann <ganteckann582006@gmail.com>
2026-06-21 14:17:15 +0800

    fix: layout client issue

--- app/buyer/layout.jsx
+++ app/buyer/layout.jsx
@@ -1,13 +1,6 @@
-"use client";
-
 import BuyerNavBar from "../_components/BuyerNavBar/BuyerNavBar";
-import { usePathname } from "next/navigation";
 
 export default function BuyerLayout({ children }) {
-  const pathname = usePathname();
-
-  if (pathname === "/buyer/chat") return children;
-
   return (
     <>
       <BuyerNavBar />
