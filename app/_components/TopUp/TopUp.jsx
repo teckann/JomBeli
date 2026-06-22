25a72b1776a29af9ffb73f5055930205ac77e460
Gan Teck Ann <ganteckann582006@gmail.com>
2026-06-22 11:42:11 +0800

    feat: top up feature (in progress)

---
+++ app/_components/TopUp/TopUp.jsx
@@ -0,0 +1,13 @@
+import styles from "./TopUp.module.css";
+
+function TopUp() {
+  return (
+    <div className={styles.topUpContainer}>
+      <p className={styles.title}>Input Top Up Amount</p>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default TopUp;
--- app/buyer/wallet/page.jsx
+++ app/buyer/wallet/page.jsx
@@ -1,8 +1,9 @@
 import WalletFilterBar from "@/app/_components/WalletFilterBar/WalletFilterBar";
 import styles from "./page.module.css";
-import { getTransactionMonths } from "@/app/_lib/data-services";
+import { getTransactionMonths, getUserInfo } from "@/app/_lib/data-services";
 import { getUser } from "@/app/_lib/auth";
 import TransactionRecords from "@/app/_components/TransactionRecords/TransactionRecords";
+import TopUp from "@/app/_components/TopUp/TopUp";
 
 export const metadata = {
   title: "Wallet",
@@ -13,7 +14,9 @@ async function page({ searchParams }) {
 
   const user = await getUser();
   const availableMonths = await getTransactionMonths(user.id);
+  const { balances } = await getUserInfo(user.id);
   // console.log(availableMonths);
+  console.log(balances);
 
   return (
     <main className={styles.main}>
@@ -22,13 +25,31 @@ async function page({ searchParams }) {
         <TransactionRecords userID={user.id} month={month} type={type} />
       </div>
 
-      <div className={styles.div2}>space 2</div>
+      <div className={styles.div2}>
+        <Wallet>
+          <WalletBalances balances={balances} />
+          <TopUp />
+        </Wallet>
+      </div>
     </main>
   );
 }
 
-const Transactions = () => {
-  return <div>Transactions...</div>;
+const Wallet = ({ children }) => {
+  return <div className={styles.wallet}>{children}</div>;
+};
+
+const WalletBalances = ({ balances }) => {
+  return (
+    <div className={styles.walletBalance}>
+      <p className={styles.walletTitle}>Balance</p>
+
+      <div className={styles.walletAmount}>
+        <span>RM</span>
+        <h2>{balances.toFixed(2)}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default page;
