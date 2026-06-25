3c3242474cb23bf71df50cbe29de966ebf1230f1
Cynth1a <cynthiatan0121@gmail.com>
2026-06-26 03:32:58 +0800

    fix: sku logic

--- app/seller/addnewproduct/page.jsx
+++ app/seller/addnewproduct/page.jsx
@@ -33,19 +33,18 @@ export default function AddNewProduct () {
 
     useEffect (() => {
         const validVariants = variants.filter (v => v.option.trim() !== '' && v.values.trim() !== '')
-
         if (validVariants.length === 0) {
             setSKU([]);
             return;
         }
+
         const validSKU = validVariants.map(v => v.values.split(',').map(val => val.trim()).filter(Boolean));
 
         if (validSKU.some(layer => layer.length === 0)) {
             setSKU([]);
             return;
         }
-
-        // Cartesian Product (to generate sku)
+      
         const generateCartesian = (arrays) => {
             return arrays.reduce((acc, curr) => {
                 const res = [];
@@ -60,12 +59,11 @@ export default function AddNewProduct () {
 
             const cartesianResults = generateCartesian(validSKU);
 
-            // make ["red", "1TB"] into "Red-1TB"
             const newCombinations = cartesianResults.map(combo => {
             const skuName = combo.join('-'); 
 
             const existing = sku.find(c => c.sku === skuName);
-        
+            
             return {
                 sku: skuName,
                 price: existing ? existing.price : '',
@@ -75,10 +73,47 @@ export default function AddNewProduct () {
             });
 
             setSKU(newCombinations);
-        }, [variants, sku]);
+            }, [variants, sku]);
+
+        const handleAddOption = () => {
+            setVariants([...variants, { option: '', values: '' }]);
+        };
+
+        const handleRemoveOption = (index) => {
+            const updated = variants.filter((_, i) => i !== index);
+            setVariants(updated.length > 0 ? updated : [{ option: '', values: '' }]);
+        };
+
+        // change data
+        const handleVariantChange = (index, field, value) => {
+            const updated = [...variants];
+            updated[index][field] = value;
+            setVariants(updated);
+        };
+
+        // when seller input price, stock and pic
+        const handleComboChange = (index, field, value) => {
+            const updated = [...combinations];
+            updated[index][field] = value;
+            setCombinations(updated);
+        };
+
+        // delete
+        const handleRemoveCombo = (index) => {
+            setCombinations(combinations.filter((_, i) => i !== index));
+        };
+
+        // not done yet
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            
+            const payload = {
+                ...coreSpec,
+                skus: combinations
+            };
+        };
 
 
 
 
-
-}
+    }
