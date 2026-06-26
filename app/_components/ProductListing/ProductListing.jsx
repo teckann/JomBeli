ac8c851b17b47137df59b552f8525c8306570284
Gan Teck Ann <ganteckann582006@gmail.com>
2026-06-27 01:09:53 +0800

    feat: home page in progress

---
+++ app/_components/ProductListing/ProductListing.jsx
@@ -0,0 +1,12 @@
+import ProductCard from "../ProductCard/ProductCard";
+import styles from "./ProductListing.module.css";
+
+function ProductListing({ products }) {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
+}
+
+export default ProductListing;
