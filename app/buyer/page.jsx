4f67c73b15a8188dbec606d07ac29b62ba4c2b08
Gan Teck Ann <ganteckann582006@gmail.com>
2026-06-27 18:59:26 +0800

    chore: bcrypt

--- app/buyer/page.jsx
+++ app/buyer/page.jsx
@@ -29,6 +29,23 @@ export default async function Home() {
         type="discountProducts"
       />
 
+      {/* top sales */}
+      <SpecialArea
+        title="Special Offers"
+        svg={
+          <svg
+            className={styles.icon}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            preserveAspectRatio="none"
+          >
+            <path d="M13 2L3 14h7l-1 8 12-14h-7l-1-6z" />
+          </svg>
+        }
+        type="discountProducts"
+      />
+
       {/* temp */}
       <div className={styles.temp}>
         <ThemeToggleButton />
