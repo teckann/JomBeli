122f9e7294d5421b38f21342c338d422e7c2db70
belugawhale0912 <leon0912tan@gmail.com>
2026-06-27 22:33:44 +0800

    feat: implemented cart page

---
+++ app/buyer/cart/page.jsx
@@ -0,0 +1,9 @@
+import BuyerCartComponent from "@/app/_components/BuyerCartComponent/BuyerCartComponent";
+
+export default function CartPage(){
+    return(
+        <div>
+            <BuyerCartComponent/>
+        </div>
+    )
+}
