60692cc92554141691a43fadea3b7a3818d95809
Gan Teck Ann <ganteckann582006@gmail.com>
2026-07-02 21:47:42 +0800

    feat: final voucher page

---
+++ app/buyer/vouchers/success/page.jsx
@@ -0,0 +1,15 @@
+import Success from "@/app/_components/Success/Success";
+
+function page() {
+  return (
+    <div>
+      <Success
+        alt="test"
+        title="Successful Redeem Voucher"
+        desc="The voucher has been succesful redeem"
+      />
+    </div>
+  );
+}
+
+export default page;
