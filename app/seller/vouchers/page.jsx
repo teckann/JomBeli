37d3629c086648d43bcca323e6045685494d287b
Jimmylim69 <limjinming0609@gmail.com>
2026-07-05 19:12:07 +0800

    chore: Voucher Image

--- app/seller/vouchers/page.jsx
+++ app/seller/vouchers/page.jsx
@@ -2,6 +2,9 @@ import VoucherTable from '@/app/_components/SellerTable/voucherTable';
 import styles from './vouchers.module.css'
 import Link from 'next/link';
 
+import Image from 'next/image';
+
+
 import { getSellerVoucher } from "@/app/_lib/data-services";
 
 import { getUsedVoucher } from "@/app/_lib/data-services";
