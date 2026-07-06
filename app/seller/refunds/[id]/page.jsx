652706da42ce85339bd73c1113c3bb1cab3f088e
Jimmylim69 <limjinming0609@gmail.com>
2026-07-06 15:55:39 +0800

    chore: seller table design improvement

--- app/seller/refunds/[id]/page.jsx
+++ app/seller/refunds/[id]/page.jsx
@@ -29,7 +29,7 @@ const Refund = async ({ params }) => {
 
     let status = 'Pending';
     let isResponse = false;
-    let statStyle = 'pending';
+    let statStyle = 'Pending';
 
     if (oneRefund.admin_status === 'Approved' || oneRefund.admin_status === 'Rejected') {
       status = oneRefund.admin_status + ' by admin';
@@ -51,6 +51,9 @@ const Refund = async ({ params }) => {
 
   return (
 <div className={styles.fcon}>
+
+  <a href="/seller/refunds" className={styles.backBtn} style={{ textDecoration: 'none' }}>← Back</a>
+
   <form action={RefundAction} className={styles.fcon}>
       
       <input type="hidden" name="refund_id" value={oneRefund.refund_id} />
@@ -67,7 +70,7 @@ const Refund = async ({ params }) => {
         
         <div className={styles.actionContainer}>
 
-          {statStyle === 'pending' && (
+          {statStyle === 'Pending' && (
                 <div className={styles.response}>
                     <button type="submit" name="action" value="Rejected" className={`${styles.btn} ${styles.btnReject}`}>
                         Reject Refund
@@ -261,7 +264,7 @@ const Refund = async ({ params }) => {
 
             <h3 className={styles.cardTitle}>Remarks</h3>
 
-            { statStyle !== 'pending' ? (
+            { statStyle !== 'Pending' ? (
               <p>{(oneRefund.seller_remarks || oneRefund.admin_remarks) || ''}</p>
             ) : (
               <textarea 
