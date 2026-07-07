adb1eae3a146d74572b44019f69dc81a27a230f4
belugawhale0912 <leon0912tan@gmail.com>
2026-07-08 03:25:00 +0800

    fix: credit / debit misconfiguration

--- app/_lib/processpayment.js
+++ app/_lib/processpayment.js
@@ -131,7 +131,7 @@ export async function processPayment({
       wallet_transaction_id: await IDGenerator(),
       user_id: buyerId,
       transaction_type: "Purchase",
-      direction: "Debit",
+      direction: "Credit",
       payment_method: "Wallet",
       amount: totalAmount,
       wallet_transaction_status: "Success",
